083db89a78ff07bc7f064a26f6bb3467e4ff8832
Mandy Robinson <mnm353@gmail.com>
2019-05-08 20:50:22 -0600

    update

---
+++ javascript/helloworld.js
@@ -0,0 +1,15 @@
+//function checkAge(age) {
+  //  var oldEnough = age >= 21 ? 'Yup' : 'Nope' ;
+    //console.log(oldEnough) ;
+  //}
+
+  function addTen( arr ) {
+    var newArr = [];
+    
+    for( var i = 0; i < arr.length; i++ ) {
+      if(type === "string") {
+         newArr.push = Number(arr);
+      }
+    return newArr[i] + 10;
+    }
+  }
